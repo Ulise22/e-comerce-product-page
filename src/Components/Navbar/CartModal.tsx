14b43eb8351039e28218afa4f3f08cd652677bf5
Ulises Montenegro <ulimontenegro23@gmail.com>
2023-01-20 18:10:20 -0300

    responsive NavbarComponent done

---
+++ src/Components/Navbar/CartModal.tsx
@@ -0,0 +1,37 @@
+import './CartModal.css';
+import thumbnail from './../Images/image-product-1-thumbnail.jpg';
+import iconDelete from './../Images/icon-delete.svg';
+
+const CartModal = ({visibleCart, handleCart}: any) => {
+    if(!visibleCart) return null;
+
+
+    return(
+        <div className='cartModal'>
+            <div onClick={(e) => {
+                e.stopPropagation()
+            }} className='cartModal__container'>
+                <div className='cartModal__title'>
+                    <header>
+                        <h4>Cart</h4>
+                    </header>
+                </div>
+
+                <section className='cartModal__product__container'>
+                    <div className='cartModal__product'>
+                        <img className='cartModal__product__img' src={thumbnail} alt="shoes" />
+                        <div className='cartModal__product__desc'>
+                            <p>Fall Limited Edition Sneakers</p>
+                            <p className='cartModal__prices'>$125.00 x 3 <b>$375.00</b></p>
+                        </div>
+                        <button className='cartModal__product__btn'><img src={iconDelete} alt="delete" /></button>
+                    </div>
+
+                    <a onClick={handleCart} className='cartModal__btn' href='#'>Checkout</a>
+                </section>
+            </div>
+        </div>
+    )
+}
+
+export default CartModal;
--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,27 +1,31 @@
 import {useState} from "react";
-import menu from "./../Images/icon-menu.svg";
+import open from "./../Images/icon-menu.svg";
 import close from './../Images/icon-close.svg';
 import logo from './../Images/logo.svg';
-import cart from './../Images/icon-cart.svg';
 import avatar from './../Images/image-avatar.png';
 import './Navbar.css';
+import CartModal from "./CartModal";
 
 const Navbar = () => {
-    const [open, setOpen] = useState(false);
+    const [menu, setMenu] = useState(false);
+    const [visibleCart, setVisibleCart] = useState(false);
+
+    const handleCart = () => {
+        setVisibleCart(!visibleCart)
+    }
 
     return(
         <nav className="navbar">
             <div className="navbar__container">
-                <div onClick={() => setOpen(!open)} className="navbar__menu">
-                    {open ? <img src={close} alt="close" /> : <img src={menu} alt="menu" />}
-                </div>
-
                 <div className="navbar__info">
+                    <div onClick={() => setMenu(!menu)} className="navbar__menu">
+                        {menu ? <img src={close} alt="close" /> : <img src={open} alt="menu" />}
+                    </div>
                     <div className="navbar__logo">
                         <img src={logo} alt="sneakers" />
                     </div>
-
-                    <ul className="navbar__list">
+                
+                    <ul className={menu ? "navbar__list active" : "navbar__list"}>
                         <li className="navbar__list__item">Collections</li>
                         <li className="navbar__list__item">Men</li>
                         <li className="navbar__list__item">Women</li>
@@ -31,14 +35,16 @@ const Navbar = () => {
                 </div>
 
                 <div className="navbar__user">
-                    <div className="navbar__user__cart">
-                        <img src={cart} alt="cart" />
+                    <div onClick={handleCart} className="navbar__user__cart">
+                        <svg className="cart" width="22" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z" fill="#69707D" fill-rule="nonzero"/></svg>
                     </div>
 
-                    <div className="navbar__user__avatar">
+                    <div onClick={handleCart} className="navbar__user__avatar">
                         <img src={avatar} alt="human" className="navbar__user__avatar__img" />
                     </div>
                 </div>
+
+                <CartModal visibleCart={visibleCart} handleCart={handleCart} />
             </div>
         </nav>
     )
